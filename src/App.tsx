197d74c87adebf1473375c5c506c6082012c36f4
mrikari <61904007+mrikari@users.noreply.github.com>
2022-11-02 23:26:01 +0900

    wip: fib card vote

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,58 @@
+import { useState } from "react";
 import "./App.css";
+import Button from "./Button";
 import Card from "./Card";
 import { fib } from "./utils";
 
 function App() {
+  const [selected, setSelected] = useState(-1);
+  const [dat, setData] = useState<{ [key: string]: number }>({
+    me: -1,
+    you: 8,
+  });
+
   const list = fib(12);
   list[0] = 0.5;
   return (
     <div className="App">
       <div className="container mx-auto border-4 border rounded overflow-hidden shadow-lg p-5">
-        <div className="gap-2 grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8">
+        <div className="gap-2 grid grid-cols-1 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8">
           {list.map((i, idx) => (
-            <Card key={idx} text={i} size="small"></Card>
+            <Card
+              onClick={() => {
+                setSelected(i);
+                setData({ ...dat, me: i });
+              }}
+              key={idx}
+              text={i}
+              size="small"
+            ></Card>
           ))}
         </div>
+      </div>
+      <hr className="m-5"></hr>
+      <div className="container p-5">
+        <Button>VOTE</Button>
+      </div>
+      <hr className="m-5"></hr>
+      <div className="container mx-auto border-4 border rounded overflow-hidden shadow-lg p-5">
+        <div className="gap-2 grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8">
+          {Object.keys(dat).map((d) => (
+            <Card key={d} text={dat[d]} size="small"></Card>
+          ))}
+        </div>
+      </div>
+      <hr className="m-5"></hr>
+      <div className="container mx-auto border-4 border rounded overflow-hidden shadow-lg p-5">
+        <p>
+          {"avg: "}
+          {Object.keys(dat).length === 0
+            ? 0
+            : Object.keys(dat).reduce(
+                (p, c) => (dat[c] === -1 ? p : p + dat[c]),
+                0
+              ) / Object.keys(dat).length}
+        </p>
       </div>
     </div>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,12 +1,16 @@
+import { useCallback } from "react";
+
 type CardProps = {
   size: "small" | "medium" | "large";
   text: string | number;
+  onClick?: React.MouseEventHandler<HTMLDivElement>
 };
 
 function Card(props: CardProps) {
-  const { text, size } = props;
+  const { text, size, onClick } = props;
+
   return (
-    <div className="flex-auto box-border h-32 w-24 border-4 border-gray-400 rounded overflow-hidden shadow-lg">
+    <div onClick={onClick} className="cursor-pointer flex-auto box-border h-32 w-24 border-4 border-gray-400 rounded overflow-hidden shadow-lg">
       <div className="px-6 py-10">
         <div className="font-bold text-xl mb-2">{text}</div>
       </div>
