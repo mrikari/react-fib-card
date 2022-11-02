d6f7e20e85eec5951bd86048ec5e778365786dc5
mrikari <>
2022-11-02 22:22:01 +0900

    add card list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./Card";
+import { fib } from "./utils";
 
 function App() {
+  const list = fib(12);
+  list[0] = 0.5;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mx-auto border-4 border rounded overflow-hidden shadow-lg p-5">
+        <div className="gap-2 grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8">
+          {list.map((i, idx) => (
+            <Card key={idx} text={i} size="small"></Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
